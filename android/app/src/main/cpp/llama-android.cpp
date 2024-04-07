b54a7dd251ddcf5cefa5b55e01202e939e57aa5f
iAkashPaul <akash@akashpaul.com>
2024-04-07 15:58:37 +0530

    reduced ctx_length to 100 tok & thread to 3

--- android/app/src/main/cpp/llama-android.cpp
+++ android/app/src/main/cpp/llama-android.cpp
@@ -116,12 +116,12 @@ Java_com_example_llama_Llm_new_1context(JNIEnv *env, jobject, jlong jmodel) {
         return 0;
     }
 
-    int n_threads = std::max(1, std::min(8, (int) sysconf(_SC_NPROCESSORS_ONLN) - 2));
+    int n_threads = 3;//std::max(1, std::min(8, (int) sysconf(_SC_NPROCESSORS_ONLN) - 2));
     LOGi("Using %d threads", n_threads);
 
     llama_context_params ctx_params = llama_context_default_params();
-    ctx_params.seed  = 1234;
-    ctx_params.n_ctx = 2048;
+    ctx_params.seed  = 1337;
+    ctx_params.n_ctx = 100;
     ctx_params.n_threads       = n_threads;
     ctx_params.n_threads_batch = n_threads;
 
